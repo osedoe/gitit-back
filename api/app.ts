20e32089a0b2663664a55980a3e4fb5fabd41f46
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2020-06-04 08:04:53 +0100

    Checkpoint

--- api/app.ts
+++ api/app.ts
@@ -1,8 +1,16 @@
 import * as express from 'express'
 import github from './routes/githubRequest';
+import * as isDev from "electron-is-dev";
+
+if (isDev) {
+    console.log('Running in development');
+} else {
+    console.log('Running in production');
+}
 
 const app = express();
 const port = process.env.PORT || 3000;
+
 
 app.use('/github', github)
 
--- api/auth/auth.ts
+++ api/auth/auth.ts
@@ -1,1 +1,7 @@
 import * as querystring from 'querystring'
+
+const options = {
+    client_id: '',
+    client_secret: '',
+    scopes: ['']
+};
