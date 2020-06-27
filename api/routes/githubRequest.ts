389c96a562af40b5de203547e567a2b1094c43f6
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2020-06-27 13:21:02 +0100

    Fix

--- api/routes/githubRequest.ts
+++ api/routes/githubRequest.ts
@@ -7,10 +7,10 @@ const router = express.Router();
 
 router.get('/notifications', (req, res, next) => {
     res.send('123');
-})
+});
 
+// eslint-disable-next-line @typescript-eslint/no-empty-function
 router.get('/login', (req, res, next) => {
-
-})
+});
 
 export default router;
